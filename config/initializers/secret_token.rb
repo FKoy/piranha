a5da21dc6df19d79d6cdf1ea5df2f123bd8cd2f1
koya <fukushi098@gmail.com>
2015-02-10 23:46:22 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Piranha::Application.config.secret_key_base = '542ce18b0a718045216003f2ebe804e4e49ad5508098d015badb0547ea49e208b79b989debf90fd6ba88830aa975bd916bdbc96ab2ebf85945da45764d1dd181'
